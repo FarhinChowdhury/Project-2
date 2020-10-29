38d111fff3aed7e68290e5978b5128bd045bff49
Dailey Kaze <daileykaze@gmail.com>
2020-10-29 15:20:10 -0400

    Fixed server & added post.js

--- app/router/router.js
+++ app/router/router.js
@@ -5,19 +5,34 @@ real controller elements are the 'router', so we create a
 router folder
 ====================================================== */
 
-const orm = require('../models/orm');
-const {Op} = require('sequelize')
+const db = require('../models');
+var path = require('path')
+const { Op } = require('sequelize')
 
 function router( app ){
+
+    //HTML routes
+    app.get('/', function(req, res){
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    })
+
+    app.get('/bids', async function(req, res){
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    })
+
+    app.get('/posts', async function(req, res){
+        res.sendFile(path.join(__dirname, '../public/products.html'))
+    })
+
     // to get all the bids
     app.get('/api/bids', async function(req, res) {
-        let result = await orm.findAll()
+        let result = await db.Post.findAll()
         res.json(result)
     })
 
     // to get bids for a specific category
     app.get('/api/posts/category/:category', async function(req, res){
-        let result = await orm.findAll({
+        let result = await db.Post.findAll({
             where: {
                 category: req.params.category
             }
@@ -27,7 +42,7 @@ function router( app ){
 
     // to get bids with title containing search query
     app.get('/api/posts/:title', async function(req, res){
-        let result = await orm.findAll({
+        let result = await db.Post.findAll({
             where:{
                 [Op.like]: `%${req.params.name}%`
             }
@@ -37,7 +52,7 @@ function router( app ){
 
     // to submit a post
     app.post('/api/posts', async function(req, res) {
-        let result = await orm.create({
+        let result = await db.Post.create({
             name: req.body.name,
             price: req.body.price,
             stock: req.body.stock,
@@ -48,7 +63,7 @@ function router( app ){
 
     // to update the current bidder
     app.put('/api/posts/id', async function(req, res) {
-        let result = await orm.update(req.body.price,
+        let result = await db.Post.update(req.body.price,
             {
                 where: {
                     id: req.body.id
@@ -59,7 +74,7 @@ function router( app ){
 
     //to delete bid
     app.delete('/api/posts/id', async function(req, res) {
-        let result = await orm.destroy({
+        let result = await db.Post.destroy({
             where: {
                 id: req.params.id
             }
