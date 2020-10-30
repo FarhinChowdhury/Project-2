28762decc8512150b7d031429823035306284cfe
Farhin Chowdhury <farhinchowdhury0@gmail.com>
2020-10-30 14:26:23 -0400

    adding things to cart

--- public/assets/script.js
+++ public/assets/script.js
@@ -27,94 +27,37 @@ let products = [
     
 ]
 
-
-
-for (i=0;i<cart.length;i++){
-    cart[i].addEventListener('click',()=>{
-        cartNumbers(products[i])
-        totalCost(products[i])
-    })
-}
 //add to cart
-function cartNumbers(product){
-    console.log(product)
-    let productNumbers= localStorage.getItem('cartNumbers');
-    productNumbers=parseInt(productNumbers)
 
-    if(productNumbers){
-        localStorage.setItem('cartNumbers',productNumbers + 1) 
-        document.querySelector("#cartCounter").textContent = productNumbers + 1; 
-    }else{
-    localStorage.setItem('cartNumbers', 1)
-    document.querySelector("#cartCounter").textContent = 1;
-    } 
-    setItems(product)
+const cartItems = localStorage.cartItems ? JSON.parse(localStorage.cartItems): []
+function addToCart(productTag){
+    console.log(`product picked`,productTag)
+    const productExsist = products.filter(item=>item.tag===productTag)
+    const product = productExsist[0]
+    cartItems.push(product)
+    
+    localStorage.cartItems = JSON.stringify(cartItems)
+    console.log(cartItems.length)
+
+
+    const cartCounter = localStorage.cartCounter = cartItems.length
+
+    document.querySelector("#cartCounter").textContent = cartCounter;
+
 }
 
-function setItems(product){
-    let cartItems = localStorage.getItem("productsPicked")
-    cartItems = JSON.parse(cartItems);
-    
 
-    if(cartItems!=null){
-        if(cartItems[product.tag] == undefined){
-            cartItems = {
-                ...cartItems,
-                [product.tag]:product
-            }
-        }
-        cartItems[product.tag].inCart +=1;
-    }else{
-        product.inCart = 1;
-        cartItems = {[product.tag]: product}
-    }
-    
-   
-    localStorage.setItem("productsPicked", JSON.stringify(cartItem))
-}
-
-//remembers previous selections into cart
 
 function remeberCartItems(){
-    let productNumbers= localStorage.getItem('cartNumbers');
-    if(productNumbers){
-        document.querySelector("#cartCounter").textContent = productNumbers
+    let cartHistory= localStorage.getItem('cartCounter');
+    if(cartHistory){
+        document.querySelector("#cartCounter").textContent = cartHistory
     }
 }
 
-function totalCost(product){
-    let cartCost = localStorage.getItem('totalCost');
-    
+function displayCartItems(){
 
-    if(carCost != null){
-        cartCost = parseFloat(cartCost)
-        localStorage.setItem('totalCost', cartCost+ product.price)
-
-    }else{
-    localStorage.setItem("totalCost", product.price)
-    }
 }
 
-function displayItems(){
-    let cartItems = localStorage.getItem("productPicked")
-    cartItems= JSON.parse(cartItems)
-    let productContainer = document.querySelector(".product")
-
-    if(cartItems && productContainer){
-        console.log("running")
-        productContainer.innerHTML ='';
-        Object.values(cartItems).map(item=>{
-            productContainer.innerHTML +=`
-            <div class="product">
-                <i class="far fa-times-circle"></i>
-                <span>${item.name}</span>
-            </div>
-            <div class="price">$${item.price}</div>
-            <input class="qantity>${item.inCart}>
-            `
-        })
-
-    }
-}
 remeberCartItems()
-displayItems()
+
