555c5deea1f7ab12626369fd8fa9ece9ac7637ed
Levi Halvorson <lhalvorson34@gmail.com>
2020-10-30 15:06:03 -0600

    made the forms better looking

--- public/assets/forms.js
+++ public/assets/forms.js
@@ -1,31 +1,32 @@
-$(document).ready(function() {
-    // Gets an optional query string from our url (i.e. ?post_id=23)
-    // var url = window.location.search;
-    // var postId;
-    // // Sets a flag for whether or not we're updating a post to be false initially
-    // var updating = false;
-
-    // If we have this section in our url, we pull out the post id from the url
-    // In localhost:8080/cms?post_id=1, postId is 1
-    // if (url.indexOf('?post_id=') !== -1) {
-    //     postId = url.split('=')[1];
-    //     getPostData(postId);
-    // }
+$(document).ready(function () {
 
     function submitProduct(Post) {
-        console.log('submitting: ',Post)
-        $.post('/api/posts', Post, function() {
-            console.log('Adding product to database...')
+        console.log('submitting: ', Post);
+        $.post('/api/posts', Post, function () {
+            console.log('Adding product to database...');
             // window.location.href = '/product';
         });
     }
 
+    function confirmNewProduct(data) {
+        $('#productName').text(data.name)
+        $('#productDesc').text(data.desc)
+        $('#productStock').text(`Stock: ${data.stock}`)
+        $('#productCat').text(data.category)
+        $('#productBid').text(`Starting Bid: ${data.price}`)
+        $('#productImage').attr('src',data.image)
+        $('#myModal').modal({show:true})
+        $('#saveBtn').on('click', submitProduct(data))
+    }
+
+
+
     var productInput = $('#product');
     var descriptionInput = $('#desc');
-    var categoryInput = $('#category')
-    var startingBidInput = $('#startingBid')
-    var stockInput = $('#stock')
-    var imageInput = $('#uploadImage')
+    var categoryInput = $('#category');
+    var startingBidInput = $('#startingBid');
+    var stockInput = $('#stock');
+    var imageInput = $('#uploadImage');
     var productForm = $('.submit');
     var postCategorySelect = $('#category');
     // Adding an event listener for when the form is submitted
@@ -33,8 +34,15 @@ $(document).ready(function() {
         event.preventDefault();
         // console.log((productInput.val()),descriptionInput.val(),categoryInput.val(),startingBidInput.val(),imageInput.val())
         // Wont submit the post if we are missing a body or a title
-        if (!productInput.val().trim() || !descriptionInput.val().trim() || !categoryInput.val() || !startingBidInput.val() || !imageInput.val() || !stockInput.val()) {
-            console.log('Unable to submit')
+        if (
+            !productInput.val().trim() ||
+      !descriptionInput.val().trim() ||
+      !categoryInput.val() ||
+      !startingBidInput.val() ||
+      !imageInput.val() ||
+      !stockInput.val()
+        ) {
+            console.log('Unable to submit');
             return;
         }
         // Constructing a newPost object to hand to the database
@@ -44,11 +52,11 @@ $(document).ready(function() {
             price: startingBidInput.val().trim(),
             stock: stockInput.val().trim(),
             category: postCategorySelect.val().trim(),
-
+            image: imageInput.val()
         };
 
         console.log(newProduct);
-        submitProduct(newProduct)
+        confirmNewProduct(newProduct);
     });
 
     // Submits a new post and brings user to blog page upon completion
