66ad154c62bacecc4e67786fd200f9d392d25ba4
Dailey Kaze <daileykaze@gmail.com>
2020-10-31 21:15:06 -0400

    Added login and signup

---
+++ app/models/user.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define('User', {
+        first_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
+        email: {
+            type:DataTypes.STRING,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: DataTypes.STRING
+    });
+
+    // validates password
+    User.prototype.validPassword = function(password){
+        return bcrypt.compareSync(password, this.password)
+    }
+
+    User.addHook('beforeCreate', function(user){
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
+    })
+
+    return User;
+};
+
